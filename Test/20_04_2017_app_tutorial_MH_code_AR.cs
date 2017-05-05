73d85d9d686f4865be424bd41cbd85d3ec164c5a
Artur <Artur>
2017-05-05 10:17:07 +0100

    Initial Project

---
+++ Test/20_04_2017_app_tutorial_MH_code_AR.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine(@"Hello World");
+
+            HttpClient client = new HttpClient();
+
+            client.GetAsync(@"http://www.google.co.uk", HttpCompletionOption.ResponseContentRead);
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            Console.Out.WriteLine(string.Format(@"Key is {0}, Value is {1}", key.KeyChar, 100));
+
+            Console.ReadKey();
+        }
+
+        private string getString()
+        {
+            return string.Empty;
+        }
+    }
+}
